d0d89c28b06e49901484a45403076a45786d3124
André <asxisto@gmail.com>
2021-01-13 02:57:27 +0000

    when product is already in cart, adding more units now increases the total units of the same product.

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -4,10 +4,10 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
-
       const existItem = state.cartItems.find(x => x.product === item.product);
 
       if (existItem) {
+        item.qty += existItem.qty;
         return {
           ...state,
           cartItems: state.cartItems.map(x => (x.product === existItem.product ? item : x))
