8952c9a6a9d9b6704f04739c40cc8d8dc780d271
André <asxisto@gmail.com>
2021-01-18 23:52:49 +0000

    user auth and profile view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import Header from './components/Header';
 import CartScreen from './views/CartView';
 import HomeView from './views/HomeView';
 import ProductView from './views/ProductView';
+import LoginView from './views/LoginView';
+import RegisterView from './views/RegisterView';
+import ProfileView from './views/ProfileView';
 
 const App = () => {
   return (
@@ -12,9 +15,12 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeView} exact />
+          <Route path='/login' component={LoginView} />
+          <Route path='/register' component={RegisterView} />
+          <Route path='/profile' component={ProfileView} />
           <Route path='/product/:id' component={ProductView} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeView} exact />
         </Container>
       </main>
       <Footer />
